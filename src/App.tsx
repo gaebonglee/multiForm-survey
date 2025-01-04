911df3d62c776887492bfd24e7d843c711e4e1f1
gayoung lee <ken604@naver.com>
2025-01-04 23:16:11 +0900

    react router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,26 @@
 import "./App.css";
 
 import MainLayout from "./components/common/MainLayout";
-import { PanelCap } from "./components/common/Panel";
-import Tabs, {
-  Tab,
-  TabList,
-  TabPanel,
-  TabPanels,
-} from "./components/common/Tabs";
 
-import SectionEditor from "./components/edit/SectionEditor";
+import SectionListEditor from "./components/edit/SectionListEditor";
+import AdminPage from "./pages/AdminPage";
 import { SurveyStoreProvider } from "./store";
+import { Routes, Route, BrowserRouter } from "react-router";
 
 function App() {
   return (
-    <MainLayout>
-      <SurveyStoreProvider>
-        <Tabs>
-          <TabList>
-            <Tab index={0}>질문</Tab>
-            <Tab index={1}>응답</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel index={0}>
-              <PanelCap>2층 1섹션</PanelCap>
-              <SectionEditor />
-            </TabPanel>
-            <TabPanel index={1}>panel2</TabPanel>
-          </TabPanels>
-        </Tabs>
-      </SurveyStoreProvider>
-    </MainLayout>
+    <BrowserRouter>
+      <MainLayout>
+        <SurveyStoreProvider>
+          <Routes>
+            <Route path="/surveys:/surveyId" element={<AdminPage />}>
+              <Route path="edit" element={<SectionListEditor />} />
+              <Route path="responses" element={<div>응답</div>} />
+            </Route>
+          </Routes>
+        </SurveyStoreProvider>
+      </MainLayout>
+    </BrowserRouter>
   );
 }
 
