3de3c324b366f3e0a8b8579774a7730c4d5e82fb
gayoung lee <ken604@naver.com>
2025-01-06 17:17:41 +0900

    recharts 를 활용하여 차트 ui 완성

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import "./App.css";
 
 import MainLayout from "./components/common/MainLayout";
 import AdminPage from "./pages/AdminPage";
+import CompletePage from "./pages/CompletePage";
 import CreatePage from "./pages/CreatePage";
 import EditPage from "./pages/EditPage";
 import FormPage from "./pages/FormPage";
+import StatisticsPage from "./pages/StatisticsPage";
 import { SurveyStoreProvider } from "./store";
 import { Routes, Route, BrowserRouter } from "react-router";
 
@@ -18,8 +20,12 @@ function App() {
             <Route path="/surveys/:surveyId" element={<FormPage />} />
             <Route path="/surveys/:surveyId" element={<AdminPage />}>
               <Route path="edit" element={<EditPage />} />
-              <Route path="responses" element={<div>응답</div>} />
+              <Route path="responses" element={<StatisticsPage />} />
             </Route>
+            <Route
+              path="/surveys/:surveyId/complete"
+              element={<CompletePage />}
+            />
           </Routes>
         </SurveyStoreProvider>
       </MainLayout>
---
+++ src/pages/StatisticsPage.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { Statistics } from "../types/app";
+import callApi from "../utils/api";
+import { useSurveyStore } from "../store";
+import SectionListStatistics from "../components/statistics/SectionListStatistics";
+
+export default function StatisticsPage() {
+  const [statistics, setStatistics] = useState<Statistics | null>(null);
+  const [count, setCount] = useState(0);
+  const { surveyId = "" } = useParams<{ surveyId: string }>();
+  const surveyStore = useSurveyStore();
+
+  useEffect(() => {
+    const fetchStatistics = async () => {
+      const { statistics, count } = await callApi<{
+        statistics: Statistics;
+        count: number;
+      }>(`/surveys/${surveyId}/statistics`);
+      setStatistics(statistics);
+      setCount(count);
+    };
+    fetchStatistics();
+    surveyStore.fetchSurvey(parseInt(surveyId, 10));
+  }, [surveyId, surveyStore]);
+
+  return statistics ? (
+    <SectionListStatistics
+      statistics={statistics}
+      count={count}
+      sections={surveyStore.sections}
+    />
+  ) : (
+    <div>Loading...</div>
+  );
+}
